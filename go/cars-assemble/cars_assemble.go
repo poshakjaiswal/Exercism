3efc756a00d5572a5b30a5f4fac3d3961182e89f
Poshak Jaiswal <poshak@greytip.com>
2022-05-10 00:11:31 +0530

    Added cars-assemble example

---
+++ go/cars-assemble/cars_assemble.go
@@ -0,0 +1,24 @@
+package cars
+
+// CalculateWorkingCarsPerHour calculates how many working cars are
+// produced by the assembly line every hour
+func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
+	//panic("CalculateWorkingCarsPerHour not implemented")
+	return (float64(productionRate) * successRate) / 100
+}
+
+// CalculateWorkingCarsPerMinute calculates how many working cars are
+// produced by the assembly line every minute
+func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
+	//panic("CalculateWorkingCarsPerMinute not implemented")
+	return int((float64(productionRate) * successRate) / 6000)
+}
+
+// CalculateCost works out the cost of producing the given number of cars
+func CalculateCost(carsCount int) uint {
+	//panic("CalculateCost not implemented")
+	batches := carsCount % 10
+	costOneCar := 95000
+	totalCost := ((carsCount-batches)*costOneCar)/10 + batches*10000
+	return uint(totalCost)
+}
--- go/weather-forecast/weather_forecast.go
+++ go/weather-forecast/weather_forecast.go
@@ -1,8 +1,13 @@
+// Package weather Tell about the weather.
 package weather
 
+// CurrentCondition reperesents the current location weather temperature.
 var CurrentCondition string
+
+// CurrentLocation reperesents the current location weather temperature.
 var CurrentLocation string
 
+// Forecast returns a string value equal telling the current weather conditions.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
