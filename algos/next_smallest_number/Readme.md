7a0ad33bec273e6cbdaa8c6eef03bfc3b8a58951
Poshak Jaiswal <poshakkj@gmail.com>
2023-05-29 15:41:12 +0530

    excercism

---
+++ algos/next_smallest_number/Readme.md
@@ -0,0 +1,13 @@
+
+https://www.linkedin.com/pulse/my-simple-coding-interview-question-wil-wen/
+
+Given a positive sorted array
+
+a = [ 3, 4, 6, 9, 10, 12, 14, 15, 17, 19, 21 ];
+
+Define a function f(a, x) that returns x, the next smallest number, or -1 for errors.
+
+i.e.
+
+f(a, 12) = 12
+f(a, 13) = 12
--- algos/next_smallest_number/next_smallest.go
+++ algos/next_smallest_number/next_smallest.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
+func nextSmallestNumber(nums1 []int, target int) int {
+
+	nums := nums1
+	sort.Ints(nums)
+
+	for _, item := range nums {
+		if target <= item {
+			return item
+		}
+	}
+
+	return -1
+
+}
 func main() {
 
+	favCards := []int{3, 4, 6, 9, 10, 12, 14, 15, 17, 19, 21}
+	target := 12
+
+	solution := nextSmallestNumber(favCards, target)
+
+	fmt.Printf("Next smallest number %d .\n", solution)
 }
